ac15e7b3f4e8921b2ae355b9362a588c24ace5ad
Satya TAMBY <tambysatya@gmail.com>
2025-06-18 09:47:38 +0200

    constrained matrix

--- LocalOptimizer.py
+++ LocalOptimizer.py
@@ -5,14 +5,17 @@ from torch_geometric.nn import GCNConv, SAGEConv, GATv2Conv, global_mean_pool, g
 
 from embedding import MLP
 from data import *
+from Blocks import *
 
 
 
 
 class Encoder (nn.Module):
-    def __init__(self, n_species, d, dropout=0.1, num_layers = 1):
+    def __init__(self, n_species, d, dropout=0.1, num_layers = 1, device=None):
         super().__init__()
-        self.h_0 = nn.Parameter(torch.randn(d)).reshape(1,1,d).repeat(num_layers, n_species, 1)
+        self.h_0 = nn.Parameter(torch.randn(n_species,d)).reshape(1,n_species,d).repeat(num_layers, 1, 1)
+        if device != None:
+            self.h_0 = self.h_0.to(device)
         self.mdl = nn.RNN(1, d, num_layers=num_layers, batch_first=True)
 
     def forward (self, x):
@@ -22,48 +25,93 @@ class Encoder (nn.Module):
 
 
 class CausalGNN (nn.Module):
-    def __init__(self, n_species, d, d_prime):
+    def __init__(self, n_species, d, d_prime, A=None, device=None):
         super().__init__()
-        self.A = nn.Parameter(torch.randn([n_species, n_species]))
+        if A == None:
+            self.A = nn.Parameter(torch.eye(n_species))
+            #self.A = nn.Parameter(torch.randn([n_species, n_species]))
+        else:
+            self.A = A
+
+        self.device = device
+        if device == None:
+            self.device = torch.device("cpu")
+
+        self.n_species = n_species
         self.W = nn.Linear(d,d_prime)
 
+        mask = 1-torch.eye(n_species)
+        self.register_buffer('mask', mask)
+
+        self.norm = nn.LayerNorm((n_species, d_prime))
+
     def forward (self, x):
-        x = torch.matmul(self.A, x)
+
+        mat = self.getMatrix()
+        x = torch.matmul(mat, x)
+        #x = torch.matmul(self.A, x)
         x = self.W(x)
+        x = F.relu(x)
+        x = self.norm(x)
         return x
+
+    def getMatrix(self):
+        mat = F.sigmoid(self.A)*self.mask
+        return mat + torch.eye(self.n_species, device=self.device)
 
 
 class CausalityInference (nn.Module):
     def __init__(self, n_species,
                  f_theta, # RNN encoder
-                 g_mu, # decoder
-                 d, d_prime # embedding dimensions
+                 g_mu, g_mu_prime, # decoder causal et decoder standard
+                 d, d_prime, # embedding dimensions
+                 device=None
                  ):
         super().__init__()
         self.n_species = n_species
         self.encoder = f_theta
-        self.decoder = g_mu
-        self.gnn = CausalGNN(n_species, d, d_prime)
+        self.decoder_causal = g_mu
+        self.decoder_std = g_mu_prime
+        self.gnn = CausalGNN(n_species, d, d_prime, device=device)
+#        self.A = CausalGNN(n_species, d, d_prime).A
+#        self.gnn = nn.Sequential (
+#                        CausalGNN(n_species, d, d_prime, A=self.A),
+#                        nn.ReLU(),
+#                        CausalGNN(n_species, d_prime, d_prime, A=self.A))
+
+
+        # pour l'apprentissage sans causal
+        self.mlps = nn.ModuleList([
+                        ResNetBlock(d, d_prime, d, dropout=0) for i in range(n_species)
+                    ])
 
     def forward(self,
                 x): #[n_species, n_timepoints]
         x = x.unsqueeze(-1)
         x = self.encoder(x) # [n_species, n_timepoints, d] : embeddings des timepoints de chaque serie
         x = x.transpose(0,1) # [n_timepoints,  n_species, d] : feature matrix at each timepoint
-        x = self.gnn(x) # [n_timepoints, n_species,d']
-        x = self.decoder(x) # [n_timepoints, n_species, 1]
 
-        x = x.transpose(0,1) # [n_species, n_timepoints]
-        return x.squeeze()
+        #translate the embeddings and use H^0 as the first embedding
+        x = x[1:] 
+        x = torch.cat((self.encoder.h_0[0].unsqueeze(0), x), dim=0)
+
+        out_causal = self.gnn(x) # [n_timepoints, n_species,d']
+        out_causal = self.decoder_causal(out_causal) # [n_timepoints, n_species, 1]
+        out_causal = out_causal.transpose(0,1) # [n_species, n_timepoints]
+
+        out_std = x.transpose(0,1) # [n_species, n_timepoints, d]
+        out_std = [mlp(out_std[i]) for i, mlp in enumerate(self.mlps)] #[n_species,n_timepoints, d']
+        out_std = torch.stack(out_std) 
+
+        out_std = self.decoder_std(out_std)
+        out_causal, out_std = out_causal.squeeze(), out_std.squeeze()
+
+        return out_causal, out_std
 
 
 
 
 
-series = torch.rand([5, 10]) # 5 species, 10 timepoints
-encoder = Encoder(5, 2) # 5 species, d=2
-decoder = nn.Linear(3,1) # d'=3
-c_mdl = CausalityInference(5, encoder, decoder, 2, 3)
 
 
 
--- main.py
+++ main.py
@@ -9,53 +9,139 @@ import torch.optim as optim
 
 
 
+device = torch.device("cuda:0") #processeur = cpu, carte graphique = cuda
 
 x = torch.load("dataset_small.pt")
 
-def compute_grad(model):
+def compute_grad(model, display=False, params_list=None):
         total_grad_norm = 0.0
-        for name, param in model.named_parameters():
-            if param.grad is not None:
-                grad_norm = param.grad.norm().item()
-                total_grad_norm += grad_norm
-                print(f" - {name} grad norm: {grad_norm:.6f}")
-        print(f" - Total gradient norm: {total_grad_norm:.6f}")
+        if params_list == None:
+            params = model.named_parameters()
+        else:
+            params = params_list
+
+        if params_list == None:
+            for name, param in params:
+                if param.grad is not None:
+                    grad_norm = param.grad.norm().item()
+                    total_grad_norm += grad_norm
+                    if display:
+                        print(f" - {name} grad norm: {grad_norm:.6f}")
+        else:
+            for param in params:
+                if param.grad is not None:
+                    grad_norm = param.grad.norm().item()
+                    total_grad_norm += grad_norm
+        if display:
+            print(f" - Total gradient norm: {total_grad_norm:.6f}")
         return total_grad_norm
 
 
 
+def fit_standard_loss(x, mdl, eps_tol=1e-2, max_epoch=100000, lr=1e-3, writer=None):
+    #optimizes only the std loss
+    optimizer = torch.optim.AdamW(#mdl.parameters(),
+                                  list(mdl.encoder.parameters())+
+                                  list(mdl.decoder_std.parameters())+
+                                  list(mdl.mlps.parameters()),
+                                  lr=lr)
 
-def fit(x, d, d_prime, eps_tol = 1e-2, num_encoder_layers=1):
-    x = x.x.squeeze()
-    n_species = x.size(0)
-    encoder = Encoder(n_species, d, num_layers=num_encoder_layers)
-    decoder = nn.Linear(d_prime,1)
-    mdl = CausalityInference(n_species, encoder, decoder, d, d_prime)
-
-    optimizer = torch.optim.AdamW(mdl.parameters(),
-                                  lr=1e-3)
-
-    writer = SummaryWriter(comment="-1")
-    for i in range(1000000000):
+    for i in range (max_epoch):
         optimizer.zero_grad()
 
         mdl.train()
-        pred = mdl(x)
+        pred_causal, pred_std = mdl(x)
 
-        loss = F.mse_loss(pred, x)
+        loss = F.mse_loss(pred_std, x)
         loss.backward()
 
         optimizer.step()
 
         loss = loss.detach().item()
 
+        if writer != None:
+            writer.add_scalar("epoch/phase1_loss", loss, i)
+
+
+        grad=compute_grad(mdl) #stoping condition on grad
+        if i % 50 == 0:
+            print ("[Phase 1] loss=",loss, " grad=", grad)
+        if grad < eps_tol:
+            return mdl, loss
+
+
+
+
+def compute_nb_params(param_list):
+    siz = 0
+    for t in param_list:
+        siz += torch.prod(torch.tensor(t.shape))
+    return siz
+
+
+
+def fit(x, d, d_prime,
+        eps_tol = 1e-2, # breakpoint
+        delta_min = 1e-3, # threshold where the causal loss is triggered
+        alpha = 1e2, #coefficient for the standard loss
+        num_encoder_layers=1):
+    x = x.x.squeeze()
+    writer = SummaryWriter(comment="-1")
+
+    n_species = x.size(0)
+    encoder = Encoder(n_species, d, num_layers=num_encoder_layers, device=device)
+    decoder_causal = nn.Linear(d_prime,1)
+    decoder_std = nn.Linear(d_prime,1)
+    mdl = CausalityInference(n_species, encoder, decoder_causal, decoder_std, d, d_prime, device=device)
+
+    mdl, x = mdl.to(device), x.to(device)
+
+    params_causal = list(mdl.gnn.parameters()) + list(mdl.decoder_causal.parameters())
+    params_std = list (mdl.mlps.parameters()) + list (mdl.decoder_std.parameters())
+
+
+    print ("nb_params causal=",compute_nb_params(params_causal), " std=", compute_nb_params(params_std))
+    mdl, delta = fit_standard_loss(x, mdl, eps_tol=delta_min, writer=writer)
+
+    
+
+    optimizer = torch.optim.Adam(#mdl.parameters(),
+                                  #list(mdl.gnn.parameters()) + list(mdl.decoder_causal.parameters()),
+                                 params_causal,
+                                 lr=1e-4)
+
+    for i in range(1000000000):
+        optimizer.zero_grad()
+
+        mdl.train()
+        pred_causal, pred_std = mdl(x)
+
+        loss_causal = F.mse_loss(pred_causal, x)
+        loss_std = F.mse_loss(pred_std, x)
+
+
+        #loss = loss_causal + alpha*F.relu(loss_std-delta)
+        loss =  loss_causal #+ mdl.gnn.A.norm(p=2)
+
+        loss.backward()
+
+        optimizer.step()
+
+        loss_causal = loss_causal.detach().item()
+        loss_std = loss_std.detach().item()
+        loss = loss.detach().item()
+
         writer.add_scalar("epoch/loss", loss, i)
+        writer.add_scalar("epoch/loss_causal", loss_causal, i)
+        writer.add_scalar("epoch/loss_std", loss_std, i)
+
+        grad=compute_grad(mdl, params_list=params_causal)
+        writer.add_scalar("epoch/phase2_grad", grad,i)
 
         if (i%50 == 0):
-            grad=compute_grad(mdl)
-            writer.add_scalar("epoch/grad", grad,i)
-            print ("Loss: ", loss)
-        if loss < eps_tol:
+            print ("[Phase2] Loss: ", loss, " causal=", loss_causal, " standard=", loss_std, " grad=", grad)
+        if grad < eps_tol:
+            print ("[Done] Loss: ", loss, " causal=", loss_causal, " standard=", loss_std, " grad=", grad)
             return mdl
 
 
